7e526511528d0cc1995e2424b720221bf58cb6cd
alexandre JACOB <143850309+alexandreJACOB9@users.noreply.github.com>
2026-01-29 14:47:04 +0100

    Add files via upload

---
+++ td-tp1-alexandreJACOB9-main/view/login.php
@@ -0,0 +1,15 @@
+<?php $title= 'Exemple Annonces Basic PHP: Connexion'; ?>
+
+<?php ob_start(); ?>
+<form method="post" action="annonces.php">
+    <label for="login"> Votre identifiant </label> :
+    <input type="text" name="login" id="login" placeholder="defaut" maxlength="12" required />
+    <br />
+    <label for="password"> Votre mot de passe </label> :
+    <input type="password" name="password" id="password" maxlength="12" required />
+
+    <input type="submit" value="Envoyer">
+</form>
+<?php $content = ob_get_clean(); ?>
+
+<?php require 'layout.php'; ?>
